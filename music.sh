c8cf3ee5da21e5d5fb64809368c64289a7810ed5
dlgeraghty <dvd.loge@gmail.com>
2020-03-29 15:56:13 +0200

    queue management

--- music.sh
+++ music.sh
@@ -2,34 +2,39 @@
 #
 # in a first version, it will only display the controls / info if there is a current song playing :)
 
-showplaylist(){
-	n_tracks=$(mpc playlist $playlist | wc -l);
-	selected=$(mpc playlist $playlist| dmenu -l $n_tracks -p "results: ${n_tracks}")&&
-		action=$(printf "play\\nremove from playlist" | dmenu -p "$selected");
+showqueue(){
+	n_tracks=$(mpc playlist | wc -l);
+	selected=$(mpc playlist | dmenu -l $n_tracks -p "results: ${n_tracks}") &&
+		lineNumber=$(mpc playlist | grep -n "$selected" | head -n 1 | cut -d: -f1);
+		action=$(printf "play\\nremove from queue" | dmenu -p "$selected : $lineNumber") &&
+			case "$action" in
+				"play") mpc play $lineNumber;;
+				"remove from queue") mpc del $lineNumber;;
+			esac
 }
 
-playlist(){
-	playlist_action=$(printf "add current song to playlist\\nsee playlist" | dmenu -i -l 2 -p "playlist actions:") &&
+queue(){
+	playlist_action=$(printf "add current song to playlist\\nsee queue" | dmenu -i -l 2 -p "queue actions:") &&
 	case "$playlist_action" in
 		"add current song to playlist");;
-		"see playlist") showplaylist;;
+		"see queue") showqueue;;
 	esac
 }
 
 if [ $(mpc current | wc -l) -gt 0 ]
 then
-	chosen=$(printf "current info\\nnext\\npause/play\\nprev\\nstop\\nplaylist" | dmenu -i -p "mympc:") &&
+	chosen=$(printf "current info\\nnext\\npause/play\\nprev\\nstop\\nqueue" | dmenu -i -p "mympc:") &&
 	case "$chosen" in
 		"current info") current=$(echo `mpc -f "%title%"`);notify-send "currently: ${current}";;
 		"pause/play") mpc toggle;;
-		"playlist") playlist ;;
+		"queue") queue ;;
 		*) mpc "$chosen";;
 	esac
 else
 	chosen=$(printf "show playlists\\nnew playlist" | dmenu -i -p "mympc:")&&
 	case "$chosen" in
 		"show playlists") playlist=$(echo $(mpc lsplaylists) | xargs -n1 echo | dmenu -i -p "choose playlist to load")&&
-			mpc load $playlist > /dev/null;mpc play > /dev/null;;
+			mpc stop; mpc clear;mpc load $playlist > /dev/null;mpc play > /dev/null;;
 		"new playlist") name=$( echo "" | dmenu -i -p "enter a name for the new playlist")&&
 			mpc stop; mpc clear;mpc save $name ;;
 	esac
